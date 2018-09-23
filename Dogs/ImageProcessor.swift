62630f44cc4badcd2598bc3c4f884803e6db871e
Oleg Semenov <olegsa@knewton.com>
2018-09-23 15:54:34 -0400

    1st Runnable app
    The net is Google's MobileNet 1.0 input size 224x224 with last layer retrained on dogs with final  test accuracy 82.5%.
    
    The code now compiles and runs in simulators and  on real iPhone 6s, but is in "horrible" state.

---
+++ Dogs/ImageProcessor.swift
@@ -0,0 +1,53 @@
+//
+//  ImageProcessor.swift
+//  Dogs
+//
+//  Created by Oleg Semenov on 9/19/18.
+//  Copyright © 2018 Ole. All rights reserved.
+//
+
+import Foundation
+import CoreVideo
+import UIKit
+
+internal struct ImageProcessor {
+    internal static func pixelBuffer (forImage uiImage:UIImage) -> CVPixelBuffer? {
+        let image = resizeImage(image: uiImage)
+        
+        let frameSize = CGSize(width: image.width, height: image.height)
+        
+        var pixelBuffer:CVPixelBuffer? = nil
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(frameSize.width), Int(frameSize.height), kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+        
+        if status != kCVReturnSuccess {
+            return nil
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
+        let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
+        let context = CGContext(data: data, width: Int(frameSize.width), height: Int(frameSize.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
+                
+        context?.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
+        
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return pixelBuffer
+    }
+    
+    private static func resizeImage(image: UIImage) -> CGImage {
+        let newSize = CGSize(width: 224, height: 224)
+        let newRect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height).integral
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        let context = UIGraphicsGetCurrentContext()!
+        context.interpolationQuality = .high
+        let flipVertical = CGAffineTransform(a: 1, b: 0, c: 0, d: -1, tx: 0, ty: newSize.height)
+        context.concatenate(flipVertical)
+        context.draw(image.cgImage!, in: newRect)
+        let newImageRef = context.makeImage()! as CGImage
+        UIGraphicsEndImageContext()
+        return newImageRef        
+    }
+    
+}
--- Dogs/ViewController.swift
+++ Dogs/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var dogImageView: UIImageView!
+    @IBOutlet weak var predictionText: UITextView!
     
     let model = mobilenet_1_0_224_dogs_1()
     
@@ -17,11 +20,73 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func chooseDogImage(_ sender: Any) {
+        let actionSheet = UIAlertController(title: "Dog Image Source", message: "Choose a source", preferredStyle: .actionSheet)
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+            if (UIImagePickerController.isSourceTypeAvailable(.camera)) {
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            } else {
+                let noCameraAlert = UIAlertController(title: "Camera not avaliable", message: nil, preferredStyle: .alert)
+                noCameraAlert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+                self.present(noCameraAlert, animated: true, completion: nil)
+            }
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default, handler: { (action) in
+            imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(actionSheet, animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        dogImageView.image = image
+        picker.dismiss(animated: true, completion: nil)
+        predict(image: image)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func predict(image: UIImage) {
+        if let pixelBuffer = ImageProcessor.pixelBuffer(forImage: image) {
+            let res = try! model.prediction(input__0: pixelBuffer)
+            predictionText.text = top7Predictions(allPredictions: res.final_result__0).description
+        }
+    }
+    
+    func top7Predictions(allPredictions: [String : Double]) -> ContiguousArray<(String, Double)> {
+        var res = ContiguousArray<(String, Double)>()
+        res.reserveCapacity(7)
+        var lastMaxScore = Double.infinity
+        var lastMaxLabel: String = ""
+        for _ in 1...7 {
+            var maxScore = -1.0
+            var maxLabel: String = ""
+            for (label, score) in allPredictions {
+                if score > maxScore && score <= lastMaxScore && label != lastMaxLabel {
+                    maxScore = score
+                    maxLabel = label
+                }
+            }
+            lastMaxScore = maxScore
+            lastMaxLabel = maxLabel
+            res.append((maxLabel, maxScore))
+        }
+        return res
+    }
 }
 
